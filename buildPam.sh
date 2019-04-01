48e12b1ad08ac2bd091eaab87e46309499d41c47
askrabal <askrabal@gmail.com>
2019-04-01 04:35:43 -0400

    Save progess

--- buildPam.sh
+++ buildPam.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+LIB_DIR=/lib/x86_64-linux-gnu/security/
+
 gcc -fPIC -fno-stack-protector -c src/mypam.c
 
-sudo ld -x --shared -o /lib/security/mypam.so mypam.o
+sudo ld -x --shared -o ${LIB_DIR}/mypam.so mypam.o
 
 rm mypam.o
